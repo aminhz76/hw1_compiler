0300abbf51e34e94382d8ab56024590db47f407a
aminhz76 <42768015+aminhz76@users.noreply.github.com>
2020-02-21 17:19:34 +0330

    update my main function and structure

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-import enum
 
-
-class Type(enum.Enum):
-    assign = '='
-    nextLine = '\n'
-    openParan = '('
-    closeParan = ')'
-    add = '+'
-    minus = '-'
-    mul = '*'
-    div = '/'
-    numberInt = int()
-    numberDouble = float()
-    var = ""
+Type = {"assign":"=", "numberDouble": float() , "numberInt":int() ,"div":"/","mul":"*","add":"+","minus":"-","closeParan":")","openParan":"(","nextLine":"\n","var":""}
 
 
 class Token:
     def __init__(self, type, value):
-        self.type = Type[str(type)].name
+        # print(type)
+        if type in Type.keys():
+            self.type = type
+        else:
+            raise ValueError('your input is incorrect')
         self.value = value
 
 
 if __name__ == "__main__":
     arr = []
-    # t = Token('var', "amin")
+    # t = Token('numberDouble', 2.2)
     # arr.append(t)
     # print(arr[0].type, arr[0].value)
 
@@ -33,28 +24,31 @@ if __name__ == "__main__":
     left = 0
     right = 0
     temp = ""
-    while (i < len(input)):
+    doubleFlag = False
+    while i < len(input):
         # print(i)
         # print("input : " + input[i])
         temp = ""
         if (input[i].isalpha()) or (input[i] == '_'):
-            # print("state")
-            # print(state)
             state = 0
+            left = i
+            right = i
+        if input[i].isdigit():
+            state = 1
             left = i
             right = i
         while state == 0:
             temp = temp + input[right]
             # print("temp :" + temp)
             right += 1
-            if (right >= len(input)):
+            if right >= len(input):
                 state = -1
                 i = right
                 t = Token('var', temp)
                 arr.append(t)
                 # print("in last char :")
                 break
-            print(right)
+            # print(right)
             if (input[right].isalpha() == False):
                 if input[right] != '_':
                     if (input[right].isdigit() == False) :
@@ -62,9 +56,50 @@ if __name__ == "__main__":
                         i = right - 1
                         t = Token('var', temp)
                         arr.append(t)
+        while state == 1 :
+            temp = temp+input[right]
+            right+=1
+            if right >= len(input):
+                if(doubleFlag == False):
+                    # print("if one :")
+                    doubleFlag = False
+                    state = -1
+                    i = right
+                    t = Token('numberInt', temp)
+                    arr.append(t)
+                    break
+                elif (doubleFlag == True):
+                    # print("if two :")
+                    doubleFlag = False
+                    state = -1
+                    i = right
+                    t = Token('numberDouble', temp)
+                    arr.append(t)
+                    break
+            # print(input[right])
+            # print(input[right].isdigit())
+            # print(doubleFlag)
+            if (input[right].isdigit() == False):
+                # print(doubleFlag)
+                if (input[right] == '.'):
+                    if (doubleFlag == False) :
+                        doubleFlag = True
+                    elif (doubleFlag == True):
+                        print("Error on : "+temp)
+                        i = len(input)
+                        break
+                elif(doubleFlag == True):
+                    state = -1
+                    i = right - 1
+                    t = Token('numberDouble', temp)
+                    arr.append(t)
+                elif(doubleFlag == False):
+                    state = -1
+                    i = right - 1
+                    t = Token('numberInt', temp)
+                    arr.append(t)
         i += 1
 
     i = 0
     for i in range(len(arr)):
         print(arr[i].type, arr[i].value)
-    # print(arr[0].type,arr[0].value)
